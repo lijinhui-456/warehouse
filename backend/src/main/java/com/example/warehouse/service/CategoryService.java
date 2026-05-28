d8f3ccb1896afea24d4e2806216b162c2eb8b0ab
lijinhui-456 <3012905795@qq.com>
2026-05-28 15:58:41 +0800

    Delete backend directory

--- backend/src/main/java/com/example/warehouse/service/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.example.warehouse.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.warehouse.entity.Category;
-
-
-public interface CategoryService extends IService<Category> {
-}
--- backend/src/main/java/com/example/warehouse/service/StockRecordService.java
+++
@@ -1,7 +0,0 @@
-package com.example.warehouse.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.example.warehouse.entity.StockRecord;
-
-public interface StockRecordService extends IService<StockRecord> {
-}
