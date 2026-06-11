fa3f2dd870d1152f40a1b1035bc2e92c48ab6baa
lijinhui-456 <3012905795@qq.com>
2026-06-11 11:25:39 +0800

    Delete backend directory

--- backend/src/main/java/com/example/warehouse/mapper/StockRecordMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.warehouse.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.warehouse.entity.StockRecord;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface StockRecordMapper extends BaseMapper<StockRecord> {
-}
